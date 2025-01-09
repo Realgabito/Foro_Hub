599785f96582fcf46bc8dc867e2af83dcbb2d634
Victor Perdomo <victorgabperdomo@gmail.com>
2025-01-08 22:15:00 -0500

    chore: add TopicoRepository for database operations / agregar TopicoRepository para operaciones con la base de datos

---
+++ src/main/java/com/realgabito/foro_hub/repository/TopicoRepository.java
@@ -0,0 +1,8 @@
+package com.realgabito.foro_hub.repository;
+
+import com.realgabito.foro_hub.dto.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
