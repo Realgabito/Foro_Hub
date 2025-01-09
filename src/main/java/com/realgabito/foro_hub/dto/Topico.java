3ab7cc470ca7a77c4aa4f554f9bede4abdfd035b
Victor Perdomo <victorgabperdomo@gmail.com>
2025-01-08 22:04:24 -0500

    chore: add Topico class to map entity attributes / agregar clase Topico para mapear atributos de la entidad

---
+++ src/main/java/com/realgabito/foro_hub/dto/Topico.java
@@ -0,0 +1,40 @@
+package com.realgabito.foro_hub.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity( name = "Topico")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private LocalDateTime fechaCreacion;
+    private Boolean status;
+    private String autor;
+    private String curso;
+    
+
+
+    public Topico(DatosRegistrarTopico datosRegistrarTopico) {
+        this.titulo = datosRegistrarTopico.titulo();
+        this.mensaje = datosRegistrarTopico.mensaje();
+        fechaCreacion = LocalDateTime.now();
+        this.status = true;
+        this.autor = datosRegistrarTopico.autor();
+        this.curso= datosRegistrarTopico.curso();
+        
+    }
+}
